61d2baf95c35225fd353109ef6dd4a4c3c8685f5
Martins, Diego Rafael <diemartins@bestbuycanada.ca>
2020-08-06 19:44:30 -0700

    extract Pet component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import { createElement } from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "pets" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
+  return createElement("div", { id: "pets" }, [
+    createElement("h1", {}, "Adopt Me!"),
+    createElement(Pet, {
       name: "Apollo",
       animal: "dog",
       breed: "German Shepherd",
     }),
-    React.createElement(Pet, {
+    createElement(Pet, {
       name: "Olive",
       animal: "cat",
       breed: "normal cat",
     }),
-    React.createElement(Pet, {
+    createElement(Pet, {
       name: "Lula",
       animal: "turtle",
       breed: "that one",
     }),
   ]);
 };
-Pet;
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(createElement(App), document.getElementById("root"));
